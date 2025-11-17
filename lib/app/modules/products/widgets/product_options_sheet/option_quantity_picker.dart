4c08b60bec9fbbf906509008ef557375fafae141
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-11-17 22:58:11 +0800

    update fix bug

--- lib/app/modules/products/widgets/product_options_sheet/option_quantity_picker.dart
+++ lib/app/modules/products/widgets/product_options_sheet/option_quantity_picker.dart
@@ -1,7 +1,6 @@
 import 'package:custom_mp_app/app/modules/products/controllers/select_variant_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 class OptionQuantityPicker extends StatelessWidget {
   final SelectVariantController controller;
   const OptionQuantityPicker({super.key, required this.controller});
@@ -10,26 +9,30 @@ class OptionQuantityPicker extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 12, top: 12),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          IconButton(
-            onPressed: controller.qty.value > 1
-                ? () => controller.qty.value--
-                : null,
-            icon: const Icon(Icons.remove_circle_outline),
-          ),
-          Obx(() => Text(
-                controller.qty.value.toString(),
-                style: Get.textTheme.titleLarge!
-                    .copyWith(fontWeight: FontWeight.bold),
-              )),
-          IconButton(
-            onPressed: () => controller.qty.value++,
-            icon: const Icon(Icons.add_circle_outline),
-          ),
-        ],
-      ),
+      child: Obx(() {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: controller.qty.value > 1
+                  ? () => controller.qty.value--
+                  : null,
+              icon: const Icon(Icons.remove_circle_outline),
+            ),
+            Text(
+              controller.qty.value.toString(),
+              style: Get.textTheme.titleLarge!.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            IconButton(
+              onPressed: () => controller.qty.value++,
+              icon: const Icon(Icons.add_circle_outline),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
+
